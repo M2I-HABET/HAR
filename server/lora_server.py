00350a44689043ca0956858b95723726efe5de2f
mjkreul <mjkreul@iastate.edu>
2021-08-09 11:26:37 -0500

    Added logger to the server script

--- server/lora_server.py
+++ server/lora_server.py
@@ -15,21 +15,13 @@ import adafruit_ssd1306
 # Import RFM9x
 import adafruit_rfm9x
 import socket
+import logging
+from datetime import datetime
 
-# Button A
-btnA = DigitalInOut(board.D5)
-btnA.direction = Direction.INPUT
-btnA.pull = Pull.UP
+# Initialize logging
+logging.basicConfig(filename='lora_server.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+logging.info("Initialized logger")
 
-# Button B
-btnB = DigitalInOut(board.D6)
-btnB.direction = Direction.INPUT
-btnB.pull = Pull.UP
-
-# Button C
-btnC = DigitalInOut(board.D12)
-btnC.direction = Direction.INPUT
-btnC.pull = Pull.UP
 
 # Create the I2C interface.
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -51,17 +43,28 @@ rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 434.0)
 rfm9x.tx_power = 23
 prev_packet = None
 print("LoRa Started")
+logging.info("LoRa Started")
 
 # Configure UDP port for information streaming
 ip = "192.168.1.205"
 port = 4440
 # Create socket for server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-# print("Do Ctrl+c to exit the program !!")
-s.connect((ip,port))
+try: 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    # print("Do Ctrl+c to exit the program !!")
+    s.connect((ip,port))
+except Exception as e :
+    logging.error("Could not initialize the server")
+    logging.error(e)
+    print("Could not initialize the server")
+    print(e)
+    s.close()
+    exit()
 
 print("Connection to server completed")
+logging.info("Connection to server completed")
 
+last_pkt_time = None
 while True:
     packet = None
     # draw a box to clear the image
@@ -73,6 +76,7 @@ while True:
     if packet is None:
         display.show()
         display.text('- Waiting for PKT -', 15, 20, 1)
+        print('Last packet received: ', last_pkt_time)
     else:
         # Display the packet text and rssi
         try :
@@ -87,40 +91,25 @@ while True:
             print(packet_text)
             # try to send the data to the penthouse machine
             try :
-                send_packet = packet_text + "," + str(bytes(rfm9x.rssi+256))
-                s.send(send_packet.encode('utf-8')) # we do not need to encode this into utf since it should already be encoded that way 
+               # send_packet = packet_text + "," + str(bytes(rfm9x.rssi+256))
+                s.send(prev_packet) # we do not need to encode this into utf since it should already be encoded that way 
                 rec_data = s.recv(4096)
                 print("Data recieved: ", rec_data.decode('utf-8'))
+                last_pkt_time = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+                print("Time: ", last_pkt_time)
             except Exception as e:
+                logging.error("\n\nUnable to send/recieve data.  Error: ")
+                logging.error(e)
                 print("\n\nUnable to send/recieve data.  Error: ")
                 print(e)
             # Send back the rssi data to the payload
-            rssi = bytes(rfm9x.rssi+256)
-            rssi_packet = rssi
-            rfm9x.send(rssi_packet)
+            #rssi = bytes(rfm9x.rssi+256)
+            #rssi_packet = rssi
+            #rfm9x.send(rssi_packet)
         except UnicodeDecodeError:
             print("\n\nPacket error\n\n")
         finally :
             time.sleep(1)
-
-    # if not btnA.value:
-    #     # Send Button A
-    #     display.fill(0)
-    #     button_a_data = bytes("Button A!\r\n","utf-8")
-    #     rfm9x.send(button_a_data)
-    #     display.text('Sent Button A!', 25, 15, 1)
-    # elif not btnB.value:
-    #     # Send Button B
-    #     display.fill(0)
-    #     button_b_data = bytes("Button B!\r\n","utf-8")
-    #     rfm9x.send(button_b_data)
-    #     display.text('Sent Button B!', 25, 15, 1)
-    # elif not btnC.value:
-    #     # Send Button C
-    #     display.fill(0)
-    #     button_c_data = bytes("Button C!\r\n","utf-8")
-    #     rfm9x.send(button_c_data)
-    #     display.text('Sent Button C!', 25, 15, 1)
 
 
     display.show()
