e3e9420966d5f0e73c784506f62c698f11bf1054
ngoeckner <goeckner@iastate.edu>
2023-10-05 10:08:51 -0500

    Revised data string format. Changed GNSS dynamic model. Added dynamic pressure sensor functionality.

--- HAR v5/main/main.ino
+++ HAR v5/main/main.ino
@@ -16,12 +16,13 @@
    Created by Nick Goeckner and Brandon Beavers
    M2I HABET
    Date Created: July 13, 2023
-   Last Updated: August 10, 2023
+   Last Updated: October 4, 2023
 */
 #include <Arduino.h>
 #include <RadioLib.h> //Click here to get the library:    https://jgromes.github.io/RadioLib/
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h> // Library found here: https://github.com/sparkfun/SparkFun_u-blox_GNSS_Arduino_Library 
 #include <Zanshin_BME680.h>
+#include <SparkFun_SDP3x_Arduino_Library.h> // Click here to get the library: http://librarymanager/All#SparkFun_SDP3x
 #include <ICM_20948.h>
 #include <Wire.h>
 
@@ -45,6 +46,7 @@
 SFE_UBLOX_GNSS GNSS;//
 BME680_Class BME680;
 ICM_20948_I2C icm20948;
+SDP3X SDP31;
 // SX1276 pin connections:
 //       | SLOT 0 | SLOT 1 |
 //==========================
@@ -76,11 +78,13 @@ void setup() {
   Serial.print(F("[NEO-M9N] Initializing..."));
   GNSS.begin();
   GNSS.setI2COutput(COM_TYPE_UBX); // Outputting UBX (U-blox binary protocol) only, no NMEA (National Marine Electronics Association)
-  GNSS.setDynamicModel(DYN_MODEL_AIRBORNE1g); // Sets dynamic model to AIRBORNE1g. Other options: PORTABLE, STATIONARY, PEDESTRIAN, AUTOMOTIVE, SEA, AIRBORNE2g, AIRBORNE4g, WRIST, BIKE
+  GNSS.setDynamicModel(DYN_MODEL_AIRBORNE2g); // Sets dynamic model to AIRBORNE2g. Other options: PORTABLE, STATIONARY, PEDESTRIAN, AUTOMOTIVE, SEA, AIRBORNE1g, AIRBORNE4g, WRIST, BIKE
   Serial.println(F("init success!"));
   delay(500);
   // ICM-20948:
   icm20948.begin(Wire,1);
+  // SPD31 (Differential Pressure Sensor):
+  SDP31.begin();
   // Radio: 
   Serial.print(F("[SX1276] Initializing ... "));
   int state = radio.begin(915.0); //-23dBm
@@ -124,55 +128,44 @@ int temp = 0;
 int pressure = 0;
 int humidity = 0;
 int gas = 0;
-// Battery Voltage:
-//float sensorRead = 0.0;
-
+// SPD31:
+float diffPressure = 0;
+float diffTemp = 0;
 void loop() {
 
   GPSLat = GNSS.getLatitude(); // divide Lat/Lon by 1000000 to get coords
-  int GPSLatInt = GPSLat/10000000.0;
-  float GPSLatFrac = (GPSLat/10000000.0) - GPSLatInt;
-  long GPSLatDec = trunc(GPSLatFrac*10000000.0);
+  //int GPSLatInt = GPSLat/10000000.0;
+  //float GPSLatFrac = (GPSLat/10000000.0) - GPSLatInt;
+  //long GPSLatDec = trunc(GPSLatFrac*10000000.0);
 
   GPSLon = GNSS.getLongitude();
-  int GPSLonInt = GPSLon/10000000.0;
-  float GPSLonFrac = ((GPSLon/10000000.0) - GPSLonInt)*-1.0;
-  long GPSLonDec = trunc(GPSLonFrac*10000000.0);
+  //int GPSLonInt = GPSLon/10000000.0;
+  //float GPSLonFrac = ((GPSLon/10000000.0) - GPSLonInt)*-1.0;
+  //long GPSLonDec = trunc(GPSLonFrac*10000000.0);
 
   GPSAlt = GNSS.getAltitude(); // measures in mm. Divide by 1000 for alt in m
-  int GPSAltInt = GPSAlt/1000.0;
-  float GPSAltFrac = (GPSAlt/1000.0) - GPSAltInt;
-  long GPSAltDec = trunc(GPSAltFrac*1000.0);
+  //int GPSAltInt = GPSAlt/1000.0;
+  //float GPSAltFrac = (GPSAlt/1000.0) - GPSAltInt;
+  //long GPSAltDec = trunc(GPSAltFrac*1000.0);
 
-  // grab time from GPS
+  // grab time from GPS - NOTE 10/4/23 - Not currently in use as requires changes to ground station tracker
   GPSHour = GNSS.getHour();
   GPSMinute = GNSS.getMinute();
   GPSSecond = GNSS.getSecond();
 
+  //get atmospheric data
   BME680.getSensorData(temp, humidity, pressure, gas);
-  // convert temp to degrees C
-  int tempInt = temp/100.0;
-  float tempFrac = (temp/100.0) - tempInt;
-  int tempDec = trunc(tempFrac*100.0);
-  // convert pressure to hPa
-  int pressInt = pressure/100.0;
-  float pressFrac = (pressure/100.0) - pressInt;
-  int pressDec = trunc(pressFrac*100.0);
-  // convert humidity to %
-  int humidInt = humidity/1000.0;
-  float humidFrac = (humidity/1000.0) - humidInt;
-  int humidDec = trunc(humidFrac*1000.0);
-
-  //sensorRead = (analogRead(4) * 3 / 4095 * 3.3 * 1.1);
-  //int vint = sensorRead;
-  //float vfrac = sensorRead - vint;
-  //int vdec = trunc(vfrac * 100.0);
-
+  
+  //get differential pressure - for pressure difference between balloon/atmosphere
+  SDP31.triggeredMeasurement();
+  delay(45);
+  SDP31.readMeasurement(&diffPressure, &diffTemp);
+  long diffPressureInt = diffPressure;
   // you can transmit C-string or Arduino string up to
   // 256 characters long
 
   char output[256];
-  sprintf(output, "$$HAR, %d, %d:%d:%d, %d.%d, %d.%d, %d.%d, %d.%d, %d.%d, %d.%d", counter++, GPSHour, GPSMinute, GPSSecond, GPSLatInt, GPSLatDec, GPSLonInt, GPSLonDec, GPSAltInt, GPSAltDec, pressInt, pressDec, tempInt, tempDec, humidInt, humidDec);
+  sprintf(output, "$$HAR, %d, %d, %d, %d, %d, %d, %d", GPSLat, GPSLon, GPSAlt, pressure, temp, humidity, diffPressureInt);
   Serial.println(output);
   int state = radio.transmit(output);
 
