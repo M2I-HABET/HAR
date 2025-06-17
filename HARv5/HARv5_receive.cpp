9f0439f2bbea76f51b8e18d5e3e2926f6b5272b6
bbeaver1 <98991345+bbeaver1@users.noreply.github.com>
2025-06-17 14:03:47 -0500

    Update HARv5_receive.cpp for RadioHead Library
    Removed RadioLib and replaced with RadioHead.

--- HARv5/HARv5_receive.cpp
+++ HARv5/HARv5_receive.cpp
@@ -1,211 +1,92 @@
 /*
-   HAR 5.0 Ground Station Receiver Code
+   HAR 5.1 Ground Station Receiver Code
 
 
-   Created by Nick Goeckner and Brandon Beavers
+   Created by Nick Goeckner and Brandon Beaver
    Based on example code by Wes Furuya
    M2I HABET
    Date Created: July 13, 2023
-   Last Updated: November 22, 2023
+   Last Updated: June 17, 2025
 
    
 */
 
-#include <RadioLib.h> //Click here to get the library: http://librarymanager/All#RadioLib
+#include <Arduino.h>
+#include <RH_RF95.h>
+#include <SPI.h>
 
-// Redefine CS Pin Name
-// SPI_CS0:     ESP32
-// SS:          ESP32, nRF, RP2040
-// SPI_CS:      Artemis
-// PIN_SPI_SS:  STM32, SAMD51, nRF, RP2040
 
-#ifndef PIN_SPI_SS
-  // For Artemis
-  #ifdef SS
-    #define PIN_SPI_SS SPI_CS
-  #endif
-  // For ESP32
-  #ifdef SPI_CS0
-    #define PIN_SPI_SS SS
-  #endif
-#endif
+// Pin definitions for SparkFun 1W LoRa Function Board (Slot A)
+int pin_cs =        5;
+int pin_dio0 =      D0;
+int pin_tx_enable = PWM0;
+int pin_rx_enable = G0;
+int pin_nrst =      G1;
+int pin_dio1 =      G2; 
 
-// SX1276 pin connections:
-//       | SLOT 0 | SLOT 1 |
-//==========================
-// cs    |   CS0  |   CS1  |
-// dio0  |   D0   |   D1   |
-// dio1  |   G2   |   G7   |
-// dio2  |   G3   |   G8   |
-// rst   |   G1   |   G6   |
-// tx_en |  PWM0  |  PWM1  |
-// rx_en |   G0   |   G5   |
+#define RF95_FREQ    915.0 // Frequency (for US)
 
-    int pin_cs =        PIN_SPI_SS;
-    int pin_dio0 =      D0;
-    int pin_tx_enable = PWM0;
-    int pin_rx_enable = G0;
-    int pin_nrst =      G1;
-    int pin_dio1 =      G2;
-  
-  SX1276 radio = new Module(pin_cs, pin_dio0, pin_nrst, pin_dio1);
+RH_RF95 rf95(pin_cs, pin_dio0);
 
 void setup() {
+
   Serial.begin(115200);
+    while (!Serial); // Wait for Serial Monitor (esp32 waits max 2 sec)
 
-  // initialize SX1276 with default settings
-  Serial.print(F("[SX1276] Initializing ... "));
-  int state = radio.begin(915.0);
-  if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("success!"));
-  } else {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
-    while (true);
+  Serial.println("Starting receiver...");
+
+  pinMode(pin_tx_enable, OUTPUT);
+  pinMode(pin_rx_enable, OUTPUT);
+  pinMode(pin_nrst, OUTPUT);
+  digitalWrite(pin_tx_enable, LOW);
+  digitalWrite(pin_rx_enable, HIGH);  // Start in RX mode
+
+  // Reset LoRa
+  digitalWrite(pin_nrst, LOW);
+  delay(10);
+  digitalWrite(pin_nrst, HIGH);
+  delay(10);
+
+  if (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    delay(1000);
+    while (1);
   }
-  radio.setOutputPower(30, true);                           // sets output power to 20 dBm and enables PA_BOOST
-  // some modules have an external RF switch
-  // controlled via two pins (RX enable, TX enable)
-  // to enable automatic control of the switch,
-  // call the following method
-  radio.setRfSwitchPins(pin_rx_enable, pin_tx_enable);
+  Serial.println("LoRa radio init OK!");
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    delay(1000);
+    while (1);
+  }
+  Serial.print("Set Freq to: ");
+  Serial.println(RF95_FREQ);
+  rf95.setTxPower(23, true);
+  rf95.setSpreadingFactor(9); // SF7..12
+  rf95.setSignalBandwidth(62500); // 62.5kHz
+  rf95.setCodingRate4(5); // 4/5
+  Serial.println("Setup complete.");
 }
-byte cmd1[] = {0x01};
-byte cmd2[] = {0x02};
-byte cmd3[] = {0x03};
-byte cmd4[] = {0x04};
-String var = "";
-int transmissionState = RADIOLIB_ERR_NONE;
+
 void loop() {
-  // Transmitting:
-  if(Serial.available() > 0){
-    var = Serial.readString();
-    var.trim();
-    if(var == "cmd"){
-      Serial.println("Select a command to send: ");
-      Serial.println("1. Blinking Light");
-      Serial.println("2. [CySat] Power Status Request");
-      Serial.println("3. [CySat] Take Measurement Request");
-      Serial.println("4. [CySat] SDR Values Request");
-      delay(1000);
-      int cmdchoice = Serial.parseInt();
-      switch (cmdchoice) {
-        case 1:
-          Serial.print(F("[SX1276] Sending packet ... "));
-          transmissionState = radio.transmit(cmd1, 1);
-          if (transmissionState == RADIOLIB_ERR_NONE) {
-            // packet was successfully sent
-            Serial.println(F("transmission finished!"));
-          } else {
-            Serial.print(F("failed, code "));
-            Serial.println(transmissionState);
-          }
-          var = "";
-          delay(500);
-          break;
-        case 2:
-          Serial.print(F("[SX1276] Sending packet ... "));
-          transmissionState = radio.transmit(cmd2, 1);
-          if (transmissionState == RADIOLIB_ERR_NONE) {
-            // packet was successfully sent
-            Serial.println(F("transmission finished!"));
-          } else {
-            Serial.print(F("failed, code "));
-            Serial.println(transmissionState);
-          }
-          var = "";
-          delay(500);
-          break;
-        case 3:
-          Serial.print(F("[SX1276] Sending packet ... "));
-          transmissionState = radio.transmit(cmd3, 1);
-          if (transmissionState == RADIOLIB_ERR_NONE) {
-            // packet was successfully sent
-            Serial.println(F("transmission finished!"));
-          } else {
-            Serial.print(F("failed, code "));
-            Serial.println(transmissionState);
-          }
-          var = "";
-          delay(500);
-          break;
-        case 4:
-          Serial.print(F("[SX1276] Sending packet ... "));
-          transmissionState = radio.transmit(cmd4, 1);
-          if (transmissionState == RADIOLIB_ERR_NONE) {
-            // packet was successfully sent
-            Serial.println(F("transmission finished!"));
-          } else {
-            Serial.print(F("failed, code "));
-            Serial.println(transmissionState);
-          }
-          var = "";
-          delay(500);
-          break;
-        default:
-          Serial.println("Please choose a valid command.");
+  if (rf95.available()) {
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+
+    if (rf95.recv(buf, &len)) {
+      Serial.print("Received [");
+      Serial.print(len);
+      Serial.print(" bytes]: ");
+      for (uint8_t i = 0; i < len; i++) {
+        Serial.write(buf[i]);
       }
-    }
-  }
-
-
-
-  // Receiving:
-  while (Serial.available() == 0){
-    //Serial.print(F("[SX1276] Waiting for incoming transmission ... "));
-
-    // you can receive data as an Arduino String
-    // NOTE: receive() is a blocking method!
-    //       See example ReceiveInterrupt for details
-    //       on non-blocking reception method.
-    String str;
-    int state = radio.receive(str);
-
-    // you can also receive data as byte array
-    /*
-    byte byteArr[8];
-    int state = radio.receive(byteArr, 8);
-    */
-
-    if (state == RADIOLIB_ERR_NONE) {
-      // packet was successfully received
-      //Serial.println(F("success!"));
-
-      // print the data of the packet
-      //Serial.print(F("[SX1276] Data:\t\t\t"));
-      Serial.print(str);
-      Serial.print(",");
-      Serial.println(radio.getRSSI());
-      // print the RSSI (Received Signal Strength Indicator)
-      // of the last received packet
-      //Serial.print(F("[SX1276] RSSI:\t\t\t"));
-      //Serial.print(radio.getRSSI());
-      //Serial.println(F(" dBm"));
-
-      // print the SNR (Signal-to-Noise Ratio)
-      // of the last received packet
-      //Serial.print(F("[SX1276] SNR:\t\t\t"));
-      //Serial.print(radio.getSNR());
-      //Serial.println(F(" dB"));
-
-      // print frequency error
-      // of the last received packet
-      //Serial.print(F("[SX1276] Frequency error:\t"));
-      //Serial.print(radio.getFrequencyError());
-      //Serial.println(F(" Hz"));
-
-    } else if (state == RADIOLIB_ERR_RX_TIMEOUT) {
-      // timeout occurred while waiting for a packet
-      //Serial.println(F("timeout!"));
-
-    } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-      // packet was received, but is malformed
-      //Serial.println(F("CRC error!"));
-
+      Serial.println();
+      Serial.print("RSSI: ");
+      Serial.print(rf95.lastRssi());
+      Serial.println(" dBm");
     } else {
-      // some other error occurred
-      //Serial.print(F("failed, code "));
-      //Serial.println(state);
+      Serial.println("Receive failed");
     }
   }
+  delay(1000);
 }
